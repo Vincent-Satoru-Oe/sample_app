d86a32d79dfaa9ca021100a40c1b0f62eee08bea
Vincent-Satoru-Oe <vincent.satoru.oe@gmail.com>
2013-05-30 09:58:58 -0700

    Finished static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,7 +9,7 @@ describe "Static pages"do
 			page.should have_selector('h1', :text => 'Sample App')
 		end
 
-		it "should have the tital 'Home'" do
+		it "should have the title 'Home'" do
 			visit '/static_pages/home'
 			page.should have_selector('title',
 				:text => "Ruby on Rails Tutorial Sample App | Home")
@@ -23,7 +23,7 @@ describe "Static pages"do
 			page.should have_selector('h1', :text => 'Help')
 		end
 
-		it "should have the tital 'Help'" do
+		it "should have the title 'Help'" do
 			visit '/static_pages/help'
 			page.should have_selector('title',
 				:text => "Ruby on Rails Tutorial Sample App | Help")
@@ -38,7 +38,7 @@ describe "Static pages"do
 			page.should have_selector('h1', :text => 'About Us')
 		end
 
-		it "should have the tital 'About Us'" do
+		it "should have the title 'About Us'" do
 			visit '/static_pages/about'
 			page.should have_selector('title',
 				:text => "Ruby on Rails Tutorial Sample App | About Us")
