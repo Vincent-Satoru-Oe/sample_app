f9c12ec5751d2a78467c415ba4d487b3a1f108c2
Vincent-Satoru-Oe <vincent.satoru.oe@gmail.com>
2013-06-04 14:03:33 -0700

    Finish sign in/sign out

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -4,20 +4,25 @@ describe "Authentication" do
 
 	subject {page}
 
-	describe "signin page" do
+	describe "signin" do
 		before {visit signin_path}
 
 		it {should have_selector('h1', text: 'Sign in')}
     	it {should have_selector('title', text: 'Sign in')}
 
-    	describe "when invalid information" do 
+    	describe "with invalid information" do 
     		before {click_button "Sign in"}
 
     		it {should have_selector('title', text: 'Sign in')}
     		it {should have_selector('div.alert.alert-error', text: 'Invalid')}
+
+            describe "after visiting another page" do
+                before {click_link "Home"}
+                it {should_not have_selector('div.alert.alert-error')}
+            end
     	end
 
-    	describe "when valid information" do
+    	describe "with valid information" do
     		let(:user) {FactoryGirl.create(:user)}
     		before do
     			fill_in "Email", with: user.email.upcase
