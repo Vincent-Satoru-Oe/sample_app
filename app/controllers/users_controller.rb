a1c213624e386bf1a7241ac6b1093e3d1a214c08
Vincent-Satoru-Oe <vincent.satoru.oe@gmail.com>
2013-06-04 17:23:04 -0700

    9.14

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,48 @@
 class UsersController < ApplicationController
+
+  before_filter :signed_in_user, only: [:edit, :update]
+
 	def show
 		@user = User.find(params[:id])
 	end
   	
-  	def new
-  		@user = User.new
-  	end
+  def new
+  	@user = User.new
+  end
 
-  	def create
-  		@user = User.new(params[:user])
-  		if @user.save
-        sign_in @user
-        flash[:success] = "Welcome to the Sample App!"
-  			redirect_to @user
-  		else
-  			render 'new'
-  		end 
-  	end
-  	
+	def create
+  	@user = User.new(params[:user])
+  	if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
+  		redirect_to @user
+  	else
+  		render 'new'
+  	end 
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user]) #I guess this changes the object in the data table for you
+      flash[:success] = "Profile Updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+    def signed_in_user
+      unless signed_in?
+        flash[:notice] = "Please sign in."
+        redirect_to signin_url
+      end
+    end
+
 end
